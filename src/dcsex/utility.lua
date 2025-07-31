a8c102cb850b0223c23ae201ba559fd5d97fea03
Jonathan Toppins <jtoppins@users.noreply.github.com>
2025-07-31 16:29:01 -0400

    utility: rename iaus to utility
    Added function comments as well.

--- src/dcsex/utility.lua
+++ src/dcsex/utility.lua
@@ -1,6 +1,7 @@
 -- SPDX-License-Identifier: LGPL-3.0
 
-local utils = require("dcsex.utils")
+--- Utility - describes an extensible utility system
+
 local class = require("dcsex.class")
 
 local curveTypes = {
@@ -15,9 +16,9 @@ local curveTypes = {
 -- @param x a number
 -- @return a float clamped between the values 0 and 1 inclusive
 local function linear(self, x)
-	x = utils.clamp(x, 0, 1)
+	x = dcsex.math.clamp(x, 0, 1)
 	local y = self.m * ((x - self.c) ^ self.k) + self.b
-	return utils.clamp(y, 0, 1)
+	return dcsex.math.clamp(y, 0, 1)
 end
 
 --- Logistic response curve described by the values set the in Axis
@@ -26,14 +27,15 @@ end
 -- @param x a number
 -- @return a float clamped between the values 0 and 1 inclusive
 local function logistic(self, x)
-	x = utils.clamp(x, 0, 1)
+	x = dcsex.math.clamp(x, 0, 1)
 	local e = 1000 * math.exp(1) * self.m ^ (-1 * x + self.c)
 	local y = (self.k / (1 + e)) + self.b
-	return utils.clamp(y, 0, 1)
+	return dcsex.math.clamp(y, 0, 1)
 end
 
 --- Describes a response curve with a clamped input range of [0,1]
 -- and a clamped output of [0,1].
+-- @type Axis
 local Axis = class("Axis")
 
 --- Constructor.
@@ -46,8 +48,8 @@ local Axis = class("Axis")
 -- @param b response curve coefficent
 -- @param c response curve coefficent
 function Axis:__init(input, curve, m, k, b, c)
-	self.inputfunc = libs.check.func(input)
-	self.curvetype = libs.check.tblkey(curve, curveTypes, "curveTypes")
+	self.inputfunc = dcsex.check.func(input)
+	self.curvetype = dcsex.check.tblkey(curve, curveTypes, "curveTypes")
 	self.m = m or 1
 	self.k = k or 1
 	self.b = b or 1
@@ -70,6 +72,7 @@ end
 
 --- Infinite Axis Utility System (IAUS). Describes a set of axes used
 -- to generate a utility score.
+-- @type IAUS
 local IAUS = class("IAUS")
 
 --- Constructor.
