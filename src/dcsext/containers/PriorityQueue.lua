9a2d05e98d3871e17c6039a2d6154472d895fca3
Jonathan Toppins <jtoppins@users.noreply.github.com>
2026-08-22 04:09:39 -0400

    docs(containers): document queue, priority queue, ring buffer and goap

--- src/dcsext/containers/PriorityQueue.lua
+++ src/dcsext/containers/PriorityQueue.lua
@@ -1,16 +1,11 @@
 -- SPDX-License-Identifier: LGPL-3.0
---[[
--- A min-heap priority queue implementation
--- API:
---   push(prio, val)
---   val, prio = pop()
---   val, prio = peek()
---   size()
---   empty()
---   remove(itemindex)
---   increase(itemindex, deltaprio)
---   decrease(itemindex, deltaprio)
---]]
+
+--- PriorityQueue - min-heap priority queue.
+-- Values are stored together with a numeric priority and are popped
+-- in ascending priority order, so the value with the lowest priority
+-- is always returned first. Create a priority queue by calling the
+-- class directly, e.g. `local pq = PriorityQueue()`.
+-- @classmod dcsext.containers.PriorityQueue
 
 local function leftchild(i)
 	return 2*i
@@ -58,10 +53,16 @@ local PriorityQueue = {
 			end
 		end,
 
+		--- Return the number of values stored in the queue.
+		-- @function PriorityQueue:size
+		-- @return the number of stored values
 		size = function(self)
 			return #self
 		end,
 
+		--- Test if the queue holds no values.
+		-- @function PriorityQueue:empty
+		-- @return bool, true when the queue is empty
 		empty = function(self)
 			if next(self) == nil then
 				return true
@@ -69,6 +70,11 @@ local PriorityQueue = {
 			return false
 		end,
 
+		--- Insert a value into the queue with the given priority.
+		-- @function PriorityQueue:push
+		-- @param p priority used to order the value, lower
+		-- priorities are popped first
+		-- @param v value to store
 		push = function(self, p, v)
 			local d = {prio = p, data = v}
 			local i = self:size()+1
@@ -77,6 +83,10 @@ local PriorityQueue = {
 			self:_siftup(i)
 		end,
 
+		--- Remove and return the value with the lowest priority.
+		-- @function PriorityQueue:pop
+		-- @return the stored value or nil when the queue is empty
+		-- @return the priority of the returned value
 		pop = function(self)
 			if self:empty() then
 				return nil
@@ -92,6 +102,11 @@ local PriorityQueue = {
 		end,
 
 
+		--- Return the value with the lowest priority without
+		-- removing it.
+		-- @function PriorityQueue:peek
+		-- @return the stored value or nil when the queue is empty
+		-- @return the priority of the returned value
 		peek = function(self)
 			if self:empty() then
 				return nil
@@ -100,19 +115,41 @@ local PriorityQueue = {
 			return self[1].data, self[1].prio
 		end,
 
+		--- Remove an item from the queue by index.
+		-- Not implemented yet, calling this method raises an
+		-- assertion error.
+		-- @function PriorityQueue:remove
+		-- @param itemindex index of the item to remove
 		remove = function(_--[[self, i]])
 			assert(false, "Not supported yet")
 		end,
 
+		--- Increase the priority of an item by a given amount.
+		-- Not implemented yet, calling this method raises an
+		-- assertion error.
+		-- @function PriorityQueue:increase
+		-- @param itemindex index of the item to change
+		-- @param deltaprio amount to add to the item priority
 		increase = function(_ --[[self, i, deltap]])
 			assert(false, "Not supported yet")
 		end,
 
+		--- Decrease the priority of an item by a given amount.
+		-- Not implemented yet, calling this method raises an
+		-- assertion error.
+		-- @function PriorityQueue:decrease
+		-- @param itemindex index of the item to change
+		-- @param deltaprio amount to subtract from the item priority
 		decrease = function(_ --[[self, i, deltap]])
 			assert(false, "Not supported yet")
 		end,
 	},
 
+	--- Constructor.
+	-- Create a new, empty priority queue by calling the class
+	-- directly, e.g. `local pq = PriorityQueue()`.
+	-- @function PriorityQueue:__call
+	-- @return a new, empty PriorityQueue instance
 	__call = function(cls)
 		return setmetatable({}, cls)
 	end
--- src/dcsext/containers/Queue.lua
+++ src/dcsext/containers/Queue.lua
@@ -1,27 +1,26 @@
 -- SPDX-License-Identifier: LGPL-3.0
---[[
--- Implements a standard doublely linked queue
--- API:
---    pushhead()
---    val pophead()
---    pushtail()
---    val poptail()
---    peekhead()
---    peektail()
---    size()
---    empty()
---    iterate()
---    riterate()
---]]
+
+--- Queue - doubly ended queue with head and tail access.
+-- Values are stored in a flat table indexed by position, giving
+-- constant time push, pop and peek operations at both ends. Create
+-- a queue by calling the class directly, e.g. `local q = Queue()`.
+-- @classmod dcsext.containers.Queue
 
 local Queue = {
 	__index = {
+		--- Push a value onto the head of the queue.
+		-- @function Queue:pushhead
+		-- @param v value to store, nil values are ignored
 		pushhead = function(self, v)
 			if v == nil then return end
 			self.head = self.head - 1
 			self[self.head] = v
 		end,
 
+		--- Pop the value at the head off the queue.
+		-- @function Queue:pophead
+		-- @return the value previously at the head or nil when the
+		-- queue is empty
 		pophead  = function(self)
 			local h = self.head
 			if h > self.tail then
@@ -33,12 +32,19 @@ local Queue = {
 			return v
 		end,
 
+		--- Push a value onto the tail of the queue.
+		-- @function Queue:pushtail
+		-- @param v value to store, nil values are ignored
 		pushtail = function(self, v)
 			if v == nil then return end
 			self.tail = self.tail + 1
 			self[self.tail] = v
 		end,
 
+		--- Pop the value at the tail off the queue.
+		-- @function Queue:poptail
+		-- @return the value previously at the tail or nil when the
+		-- queue is empty
 		poptail  = function(self)
 			local t = self.tail
 			if self.head > t then
@@ -50,14 +56,25 @@ local Queue = {
 			return v
 		end,
 
+		--- Return the value at the head without removing it.
+		-- @function Queue:peekhead
+		-- @return the value at the head or nil when the queue is
+		-- empty
 		peekhead = function(self)
 			return self[self.head]
 		end,
 
+		--- Return the value at the tail without removing it.
+		-- @function Queue:peektail
+		-- @return the value at the tail or nil when the queue is
+		-- empty
 		peektail = function(self)
 			return self[self.tail]
 		end,
 
+		--- Return the number of values stored in the queue.
+		-- @function Queue:size
+		-- @return the number of stored values
 		size     = function(self)
 			local val = self.tail - self.head + 1
 
@@ -67,10 +84,18 @@ local Queue = {
 			return val
 		end,
 
+		--- Test if the queue holds no values.
+		-- @function Queue:empty
+		-- @return bool, true when the queue is empty
 		empty    = function(self)
 			return self.head > self.tail
 		end,
 
+		--- Iterate over the values in the queue from head to tail.
+		-- @function Queue:iterate
+		-- @return an iterator function for use in a for-in loop,
+		-- yielding the index and value of each entry; iterating an
+		-- empty queue yields nothing
 		iterate  = function(self)
 			if self:empty() then
 				return function() end, nil, nil
@@ -90,6 +115,11 @@ local Queue = {
 			return fnext, self, nil
 		end,
 
+		--- Iterate over the values in the queue from tail to head.
+		-- @function Queue:riterate
+		-- @return an iterator function for use in a for-in loop,
+		-- yielding the index and value of each entry in reverse
+		-- order; iterating an empty queue yields nothing
 		riterate = function(self)
 			if self:empty() then
 				return function() end, nil, nil
@@ -110,6 +140,11 @@ local Queue = {
 		end,
 	},
 
+	--- Constructor.
+	-- Create a new, empty queue by calling the class directly,
+	-- e.g. `local q = Queue()`.
+	-- @function Queue:__call
+	-- @return a new, empty Queue instance
 	__call = function(cls)
 		return setmetatable({head = 0, tail = -1}, cls)
 	end,
