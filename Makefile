jtoppins/lua-libs

commit 87f8e0397a1c2721791ea39b02e5dda11921d69d

change default install path
Before this change the install path was
  `<DCS_SAVED_GAMES>/Scripts/mission/dcsext`

Now it is simply `<DCS_SAVED_GAMES>/Scripts/dcsext`.

Makefile
@@ -75,7 +75,6 @@ LUADOC_OPTS           = $(if $(Q),-q) -i
 TZ                    = "UTC 0"
 DISTPREFIX            := .build
 SCRIPTS_INSTALL_PATH  = $(INSTALLPREFIX)Scripts
-MISSION_LIB_PATH      = $(SCRIPTS_INSTALL_PATH)/mission
 CONFIGS_INSTALL_PATH  = $(INSTALLPREFIX)Config
 
 export PREFIX PROJ_VERSION
@@ -165,14 +164,14 @@ quiet_cmd_distzip     = ZIP     $@
 
 quiet_cmd_mod_remove  = RM      $@
       cmd_mod_remove  = \
-		rm -rf $(MISSION_LIB_PATH)/$(MODNAME)* \
+		rm -rf $(SCRIPT_INSTALL_PATH)/$(MODNAME)* \
 			$(SCRIPTS_INSTALL_PATH)/loadplugins.lua
 
 quiet_cmd_mod_install = INSTALL $@
       cmd_mod_install = \
-		mkdir -p $(MISSION_LIB_PATH); \
+		mkdir -p $(SCRIPTS_INSTALL_PATH); \
 		mkdir -p $(CONFIGS_INSTALL_PATH); \
-		cp -aL "$(srctree)"/src/* $(MISSION_LIB_PATH); \
+		cp -aL "$(srctree)"/src/* $(SCRIPTS_INSTALL_PATH); \
 		$(INSTALL) $(INSTALLFLAGS) -m 644 -t $(SCRIPTS_INSTALL_PATH) \
 			"$(srctree)"/scripts/loadplugins.lua; \
 		$(INSTALL) $(INSTALLFLAGS) --backup=numbered -m 644 \

scripts/loadplugins.lua
@@ -3,7 +3,7 @@
 --- Load script modules into the mission environment before it is sanatized.
 
 local sep      = package.config:sub(1,1)
-local libspath = table.concat({lfs.writedir(), "Scripts", "mission"}, sep)
+local libspath = table.concat({lfs.writedir(), "Scripts"}, sep)
 local pkgpath  = table.concat({libspath, "?.lua"}, sep)
 
 if lfs.attributes(libspath) == nil then

commit 4bb5ab91a6909e378fba325f36a3af610a7c49db

Makefile: fix remove target
The remove target would not actually remove the mod due to a
misspelling of the install scripts variable.

Fixes: 87f8e0397a1c ("change default install path")

Makefile
@@ -164,7 +164,7 @@ quiet_cmd_distzip     = ZIP     $@
 
 quiet_cmd_mod_remove  = RM      $@
       cmd_mod_remove  = \
-		rm -rf $(SCRIPT_INSTALL_PATH)/$(MODNAME)* \
+		rm -rf $(SCRIPTS_INSTALL_PATH)/$(MODNAME)* \
 			$(SCRIPTS_INSTALL_PATH)/loadplugins.lua
 
 quiet_cmd_mod_install = INSTALL $(MODNAME) PREFIX=$(PREFIX)